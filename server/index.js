095b572af71416ed2a5718c31adafb8e5cca0106
Kuddleman <edl1155@gmail.com>
2020-12-12 17:48:08 -0800

    set up basic structure of server/index.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+import postRoutes from './routes/posts'
+
+const app = express()
+
+// Middleware
+app.use('/posts', postRoutes)
+
+// Set up bodyParser to be able to send requests.
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:@Airwestf27@sandbox.kumrl.mongodb.net/Sandbox?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+    .catch( error => console.log(error.message))
+
+    mongoose.set('useFindAndModify', false)
+
