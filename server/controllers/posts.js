8e894b768640d972594d8ace5869d89a10640d9d
Kuddleman <edl1155@gmail.com>
2020-12-13 14:37:41 -0800

    Add logic for PostMessage controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,11 @@
 import PostMessage from '../models/postMessage.js'
 
-export const getPosts = (req, res) =>{
-   
+export const getPosts = async (req, res) =>{
+   try {
+       const postMessages = await PostMessage.find()
+   } catch (error) {
+       res.status(404).json({ message: error.message })
+   }
 }
 
 export const createPost = (req, res) => {
