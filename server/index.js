aa72930729e457a175591710af46236d503e4390
Kuddleman <edl1155@gmail.com>
2020-12-13 14:35:14 -0800

    Set up file system for controllers, models

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
+
+
 connectDB()
 const PORT = process.env.Port || 3000
 
